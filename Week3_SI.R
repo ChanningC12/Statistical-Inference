af39e162fd34f2ea38117b57f33eaaff5cfb04b6
ChanningC12 <chicheng2015@u.northwestern.edu>
2016-04-16 09:10:03 -0500

    week3

---
+++ Week3_SI.R
@@ -0,0 +1,18 @@
+sp = sqrt((7*15.34^2 + 20*18.23^2)/(8+21-2))
+132.86 - 127.44 + c(-1,1)*qt(.975,27)*sp*(1/8 + 1/21)^0.5
+
+g1 = c(112,142,134,128,131,134,129,140)
+g2 = c(123,136,129,125,124,128,134,134)
+
+n1 = length(g1)
+n2 = length(g2)
+sp = sqrt(((n-1)*sd(x1)^2) + (n2-1)*sd(x2)^2) / (n1+n2-2))
+mean(g2)
+mean(g1)
+md = mean(g2) - mean(g1)
+semd = sp * sqrt(1/n1 + 1/n2)
+rbind(
+md + c(-1,1)*qt(0.975,n1+n2-2)*semd,
+t.test(g2,g1,paired = FALSE, var.equal = TRUE)$conf,
+t.test(g2,g1,paired = TRUE)$conf
+    )
