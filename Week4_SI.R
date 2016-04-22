9302699570e38b9afaad1706bf063a270e4fe6bd
ChanningC12 <chicheng2015@u.northwestern.edu>
2016-04-21 20:35:43 -0500

    update

--- Week4_SI.R
+++ Week4_SI.R
@@ -13,14 +13,28 @@ power.t.test(n=16,delta=2,sd=4,type="one.sample",alt="one.side")$power  # delta 
 power.t.test(n=16,power=0.8,sd=4,type="one.sample",alt="one.side")$delta
 
 
-# Bootstrap
+# Bootstrap, use the distribution by the observed data to approximate its sampling distribution
 x = father.son$sheight
 n = length(x)
 B = 10000
 resample = matrix(sample(x,n*B,replace=T),B,n)
 resampledMedians = apply(resample,1,median)
 plot(density(resampledMedians),col="red")
+abline(v=median(resampledMedians))
+sd(resampledMedians)
+quantile(resampledMedians,c(0.025,0.975))
 
+library(ggplot2)
+# Estimate median distribution of the sampling
+g = ggplot(data.frame(medians = resampledMedians),aes(x=medians))
+g = g + geom_histogram(color = "black", fill = "lightblue", binwidth = 0.05)
+g
 
-
-
+# Group Comparison, Permutation Test
+subdata = InsectSprays[InsectSprays$spray %in% c("B","C"),]
+y = subdata$count
+group = as.character(subdata$spray)
+testStat = function(w,g) mean(w[g == "B"]) - mean(w[g == "C"])
+observedStat = testStat(y,group)
+permutations = sapply(1:10000,function(i) testStat(y,sample(group)))
+observedStat
